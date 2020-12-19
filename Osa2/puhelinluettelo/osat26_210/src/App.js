57bcbf03816bb8378e2ebfeb2a13f4c31ec88b75
Antti Turunen <asturune@hotmail.com>
2020-12-19 09:52:38 +0200

    osa2.11_step6_lisätty

--- Osa2/puhelinluettelo/osat26_210/src/App.js
+++ Osa2/puhelinluettelo/osat26_210/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 //Henkilön renderöimiseen webbisivulle
 const RenderPersons = ({ persons }) => {
@@ -24,16 +25,24 @@ const FilterPersons = ({ persons, howToFilter }) => {
 
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { id: 1, name: 'Arto Hellas', number: '040-123456' },
-        { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
-        { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
-        { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ])
+    const [persons, setPersons] = useState([])
     //Muuttujia jotka toimivat dynaamisesti change
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [howToFilter, setHowToFilter] = useState('')
+
+    //Tällä haetaan data db.json filestä ja asetetaan persons
+    //taulukkoon
+    useEffect(() => {
+        console.log('effect')
+        axios
+          .get('http://localhost:3001/persons')
+          .then(response => {
+            console.log('promise fulfilled')
+            setPersons(response.data)
+          })
+      }, [])
+    
 
     const addName = (event) => {
         //Estää lomakkeen lähetyksen oletusarvoisen toiminnan, 
